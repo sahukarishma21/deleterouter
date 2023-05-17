af7d7018c8f1918ec860268d3d3cc7122dba02c4
Karishma Sahu <sahukarishma21@gmail.com>
2023-05-17 10:49:43 +0530

    "solutionn"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import User from './component/Delete/Delete';
+function App() {
+  return (
+    <div className="App">
+     <User/>
+    </div>
+  );
+}
+
+export default App;
